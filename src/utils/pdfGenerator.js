be7933b0fd20d0be3a965a4b371902eb1493d362
Modit Tyagi <modittyagi50@gmail.com>
2026-03-22 02:03:38 +0530

    Add files via upload

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -108,16 +108,18 @@ export async function generatePDF({ client, metaData, ga4Data, reportId }) {
     </html>
   `
   
-  const blob = new Blob([html], { type: 'text/html' })
-  const url = URL.createObjectURL(blob)
+  const iframe = document.createElement('iframe')
+  iframe.style.cssText = 'position:absolute;width:800px;height:600px;top:-9999px;left:-9999px;border:none;'
+  document.body.appendChild(iframe)
   
-  const printWindow = window.open('', '_blank')
-  printWindow.document.write(html)
-  printWindow.document.close()
+  const iframeDoc = iframe.contentDocument || iframe.contentWindow.document
+  iframeDoc.open()
+  iframeDoc.write(html)
+  iframeDoc.close()
   
   await new Promise(resolve => setTimeout(resolve, 1500))
   
-  const canvas = await html2canvas(printWindow.document.body, { scale: 2, useCORS: true })
+  const canvas = await html2canvas(iframeDoc.body, { scale: 2, useCORS: true })
   const imgData = canvas.toDataURL('image/png')
   
   const pdf = new jsPDF('p', 'mm', 'a4')
@@ -125,8 +127,7 @@ export async function generatePDF({ client, metaData, ga4Data, reportId }) {
   const pdfHeight = (canvas.height * pdfWidth) / canvas.width
   
   pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight)
-  printWindow.close()
-  URL.revokeObjectURL(url)
+  document.body.removeChild(iframe)
   
   return pdf.output('blob')
 }
